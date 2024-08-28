7bcff0513888dd34d2508785348cdd035b436b56
desssperado <gamzatgasanov0@gmail.com>
2024-08-28 22:48:49 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,21 @@
+from operator import truediv
+
+my_dict = {'Anton': 1989, 'Max': 1999, 'Liza': 2000}
+print(my_dict)
+print(my_dict['Anton'])
+print(my_dict.get('Denis'))
+my_dict.update({'Oled': 1995, 'Olga': 1993})
+del my_dict['Liza']
+print(my_dict)
+
+my_set = {1, 'Виноград', 34.324, 34.324, 1, 'Виноград'}
+my_set = set(my_set)
+print(my_set)
+my_set.add(34), my_set.add((7, 8, 9))
+my_set.remove(1)
+print(my_set)
+
+
+
+
+
