31b7c417f74efbe7a5ccda7292f3678481a7cb9d
desssperado <gamzatgasanov0@gmail.com>
2024-09-12 22:55:05 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -6,6 +6,7 @@ print(my_dict['Anton'])
 print(my_dict.get('Denis'))
 my_dict.update({'Oled': 1995, 'Olga': 1993})
 del my_dict['Liza']
+print(my_dict.pop('Max'))
 print(my_dict)
 
 my_set = {1, 'Виноград', 34.324, 34.324, 1, 'Виноград'}
